8049f2314c21bd685be0dd0899cb41428ab3f1ba
Willcky <3076023745@qq.com>
2022-09-08 22:05:28 -0400

    controller
    save controller and get controller

---
+++ backend/src/main/java/com/team2/travelplanner/controller/GetController.java
@@ -0,0 +1,25 @@
+package com.team2.travelplanner.controller;
+
+
+import com.team2.travelplanner.entity.TravelList;
+//import com.willcky.onlineOrder.entity.Restaurant;
+//import com.willcky.onlineOrder.service.MenuInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+import java.util.List;
+
+@Controller
+public class GetController {
+
+//    @Autowired
+//    private TraverlListService TraverlListService;
+
+    @RequestMapping(value = "/user/{userId}/List", method = RequestMethod.GET)
+    @ResponseBody
+    public List<TravelList> getList(@PathVariable("customerId") int customerId) {
+//        return traverlListService.getAllListItem(customerId);
+    }
+}
---
+++ backend/src/main/java/com/team2/travelplanner/controller/SaveController.java
@@ -0,0 +1,25 @@
+package com.team2.travelplanner.controller;
+
+
+import com.team2.travelplanner.entity.Customer;
+import com.team2.travelplanner.entity.TravelList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class SaveController {
+
+//    @Autowired
+//    private CustomerService customerservice;
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void save(@RequestBody TravelList travelList, Customer customer){
+//        customerservice.saveTravelList(travelList);
+    }
+}
